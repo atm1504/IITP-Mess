1ca9821746aab17d7db0d8c3a14c7afb87186001
shreyaswikriti <57635473+shreyaswikriti@users.noreply.github.com>
2019-12-27 09:52:01 +0530

    admin login page modified

--- admin.php
+++ admin.php
@@ -9,6 +9,8 @@ if(admin_logged_in() == true){
 	<title>IIT-P Hostel Admin Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/iconstyle.css">
+
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/icons/logo.png"/>
 <!--===============================================================================================-->
@@ -35,28 +37,35 @@ if(admin_logged_in() == true){
 <body>
 	
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('images/hostel.jpg');">
+		<div class="container-login100" style="background-image: url('images/hostel1.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					Hostel Admin Account Login
 				</span>
+				 <img src="images/loginimage.jpeg" class="image">
+
+				
 				<form class="login100-form validate-form p-b-33 p-t-5" method="POST" action="#">
 					 <?php validate_admin_login() ?>
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter email id">
-						<input class="input100" type="text" name="email" id="email" placeholder="abc@xyz.com">
+						<input class="input100" type="text" name="email" id="email" placeholder="Email id">
 						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
 						<input class="input100" type="password" name="password" id="password" placeholder="Password">
-						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
+						<span  class="focus-input100" data-placeholder="&#xe80f;" ></span>
 					</div>
 
 					<div class="container-login100-form-btn m-t-32">
 						<button class="login100-form-btn">
 							Login
 						</button>
+
+					</div>
+					<div class="container-login100-form-btn" >
+						<a href="#" >Forget password</a>
 					</div>
 
 				</form>
