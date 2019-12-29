60cb2b774730af31a7b1d98a110cbdc121e0387e
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-29 21:12:33 +0530

    feat: Fix

--- includes/admin_nav.php
+++ includes/admin_nav.php
@@ -19,7 +19,7 @@
       <a class="nav-link" href="#">Change Password</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="adminLogout">Logout</a>
+      <a class="nav-link" href="adminLogout.php">Logout</a>
     </li>
   </ul>
 </nav>
