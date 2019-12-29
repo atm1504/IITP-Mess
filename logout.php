464d69dd715ddd91154bbf5e50c29aae7221164f
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-30 02:31:12 +0530

    feat: CHnage users password

--- logout.php
+++ logout.php
@@ -1,18 +1,4 @@
 <?php include("./functions/init.php");
-
-$rollno=$_SESSION['rollno'];
-$access_token=$_SESSION['access_token'];
-$sql="UPDATE users set access_token='' WHERE rollno='$rollno' and access_token='$access_token'";
-$result=query($sql);
-
-session_destroy();
-if(isset($_COOKIE['rollno'])){
-	unset($_COOKIE['rollno']);
-	setcookie('rollno','',time()-86400);
-}
-if(isset($_COOKIE['access_token'])){
-	unset($_COOKIE['access_token']);
-	setcookie('access_token','',time()-86400);
-}
-redirect("login.php");
+	logoutUser();
+	session_destroy();
 ?>
