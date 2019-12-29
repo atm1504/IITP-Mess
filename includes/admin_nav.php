9a3083d124df644085ac9676d4e8fb84c6960f26
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-29 21:07:53 +0530

    style: Enhance icon

--- includes/admin_nav.php
+++ includes/admin_nav.php
@@ -15,5 +15,8 @@
     <li class="nav-item">
       <a class="nav-link" href="#">Update Mess Rebate</a>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Change Password</a>
+    </li>
   </ul>
 </nav>
--- showComp.php
+++ showComp.php
@@ -17,6 +17,7 @@ if(admin_logged_in() == false){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 
@@ -36,6 +37,7 @@ if(admin_logged_in() == false){
       <th scope="col">Date</th>
       <th scope="col">Complain</th>
       <th scope="col">Status</th>
+      <th scope="col">Edit</th>
     </tr>
   </thead>
   <tbody>
@@ -47,6 +49,7 @@ if(admin_logged_in() == false){
         <td><?php echo $ca['date_time']; ?></td>
         <td><?php echo $ca['complain']; ?></td>
         <td><?php echo $ca['status']; ?></td>
+ 				<td><a href="#"><i class="fa fa-edit" style="font-size:24px; height:20px; width:20px"></i></a></td>
     </tr>
     <?php } ?>
         </tbody>
