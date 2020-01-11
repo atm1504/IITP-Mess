3f1ce6846ffff372937f2d9f4e29ef4b9c8e7264
shreyaswikriti <57635473+shreyaswikriti@users.noreply.github.com>
2020-01-12 02:29:35 +0530

    created navbar for users

---
+++ includes/user_nav.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="images/icons/logo.png" alt="logo" style="width:40px;">
+  </a>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    
+    <li class="nav-item">
+      <a class="nav-link" href="#">HOME</a>
+    </li>
+  
+    
+    <li class="nav-item">
+      <a class="nav-link" href="profile.php">PROFILE</a>
+    </li>
+  
+  
+    <li class="nav-item">
+      <a class="nav-link" href="complain.php">FILE COMPLAIN</a>
+    </li>
+  
+  
+    <li class="nav-item">
+      <a class="nav-link" href="pass.php">CHANGE PASSWORD</a>
+    </li>
+  
+  
+    <li class="nav-item">
+      <a class="nav-link" href="logout.php">LOGOUT</a>
+    </li>
+  
+  </ul>
+</nav>
